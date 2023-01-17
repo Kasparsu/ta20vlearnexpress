da63797472147d31c1ef2674d389886ada23a689
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-01-17 11:45:37 +0200

    add form and register post route

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const port = 3000;
 
 app.set('views', './views');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 nunjucks.configure('views', {
     autoescape: true,
     express: app
@@ -20,13 +23,26 @@ app.get('/', async (request, response) => {
   const users = await prisma.user.findMany();
   await prisma.$disconnect()
   console.log(users);
-  response.render('index.njk');
+  response.render('index.njk', { users });
 });
 
-app.get('/page1', (request, response) => {
-    response.render('index.njk');
+app.get('/register', (request, response) => {
+    console.log(request.query);
+    response.render('register.njk');
 });
 
+app.post('/register', async (request, response) => {
+  console.log(request.body);
+  const user = await prisma.user.create({
+    data: {
+      name: request.body.name,
+      email: request.body.email,
+    },
+  });
+  await prisma.$disconnect();
+  console.log(user)
+  response.redirect('/')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
