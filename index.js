3fd69f402f0cf06701742723f5f8dbde060b5f44
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-01-23 10:57:17 +0200

    add sessions

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ import express from 'express';
 import nunjucks from 'nunjucks';
 import path from 'path';
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+
+const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
 
 app.set('views', './views');
+//app.set('trust proxy', 1);
 
+app.use(session({
+  secret: 'keyboard cat',
+}));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -28,20 +36,33 @@ app.get('/', async (request, response) => {
 
 app.get('/register', (request, response) => {
     console.log(request.query);
-    response.render('register.njk');
+    response.render('register.njk', {error: request.session.error});
 });
 
 app.post('/register', async (request, response) => {
   console.log(request.body);
-  const user = await prisma.user.create({
-    data: {
-      name: request.body.name,
-      email: request.body.email,
-    },
-  });
-  await prisma.$disconnect();
-  console.log(user)
-  response.redirect('/')
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: request.body.name,
+        email: request.body.email,
+      },
+    });
+    await prisma.$disconnect();
+    console.log(user)
+    response.redirect('/')
+  } catch(e) {
+    console.log(e.meta.target);
+    let error = {
+      message: e.message.match(/Unique constraint failed on the fields: \(`\w+`\)/g),
+      field: e.meta.target
+    }
+    if(error.message){
+      request.session.error = error;
+    }
+    response.redirect('/register')
+  }
+  
 });
 
 app.listen(port, () => {
